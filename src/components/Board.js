ebc1c191a1a1b09e02fca2dc732919c73fb219bc
Alvin Patrick Bilog <balvinpatrick@yahoo.com>
2021-11-02 18:57:10 +0400

    1st deploy

---
+++ src/components/Board.js
@@ -0,0 +1,100 @@
+import { useState, useEffect } from "react";
+import "../styles/Board.css";
+import SingleCard from "./SingleCard";
+
+const cardImages = [
+  { src: "/img/helmet-1.png", matched: false },
+  { src: "/img/potion-1.png", matched: false },
+  { src: "/img/ring-1.png", matched: false },
+  { src: "/img/scroll-1.png", matched: false },
+  { src: "/img/shield-1.png", matched: false },
+  { src: "/img/sword-1.png", matched: false },
+];
+
+function Board() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
+  const [disabled, setDisabled] = useState(false);
+
+  // shuffle cards for new game
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
+  // handle a choice
+  const handleChoice = (card) => {
+    console.log(card);
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+
+  // compare 2 selected cards
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      setDisabled(true);
+
+      if (choiceOne.src === choiceTwo.src) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          });
+        });
+        resetTurn();
+      } else {
+        setTimeout(() => resetTurn(), 1000);
+      }
+    }
+  }, [choiceOne, choiceTwo]);
+
+  // reset choices & increase turn
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prevTurns) => prevTurns + 1);
+    setDisabled(false);
+  };
+
+  // start new game automagically
+  useEffect(() => {
+    shuffleCards();
+  }, []);
+
+  return (
+    <div className="Board">
+      <h1>Trolls Memory Game</h1>
+      <div className="info-container">
+        <button onClick={shuffleCards}>New Game</button>
+      </div>
+      <div className="score-container">
+        <p className="score"> {turns}</p>
+        <p className="score-text">Turns</p>
+      </div>
+
+      <div className="card-grid">
+        {cards.map((card) => (
+          <SingleCard
+            key={card.id}
+            card={card}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Board;
---
+++ src/components/SingleCard.js
@@ -0,0 +1,24 @@
+import "../styles/SingleCard.css";
+
+export default function SingleCard({ card, handleChoice, flipped, disabled }) {
+ 
+  const handleClick = () => {
+    if (!disabled) {
+      handleChoice(card);
+    }
+  };
+
+  return (
+    <div className="card">
+      <div className={flipped ? "flipped" : ""}>
+        <img className="front" src={card.src} alt="card front" />
+        <img
+          className="back"
+          src="/img/cover.png"
+          onClick={handleClick}
+          alt="cover"
+        />
+      </div>
+    </div>
+  );
+}
